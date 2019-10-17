6b4b8956b109943ff1c843d48f184cec88df7f5e
Sashfish <dormiii4@gmail.com>
2019-10-17 12:36:19 +0100

    fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,7 @@
 class DashboardController < ApplicationController
   def index
-
     @recipes = Recipe.all
     @users = User.all
-
     @recipescount = Recipe.all.count
     @userscount = User.all.count
   end
