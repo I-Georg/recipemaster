d2d081a6db08aaa5ed8fe7cb8db7cc72e589b724
Sashfish <dormiii4@gmail.com>
2019-10-17 23:36:19 +0100

    implemented cocktail api, just needs some prettying up

---
+++ app/controllers/livedata_controller.rb
@@ -0,0 +1,30 @@
+require 'unirest'
+class LivedataController < ApplicationController
+def index
+  @response1 = Unirest.get "https://the-cocktail-db.p.rapidapi.com/random.php",
+  headers:{
+    "X-RapidAPI-Host" => "the-cocktail-db.p.rapidapi.com",
+    "X-RapidAPI-Key" => "bcd50a1ebdmsh7fc3a20a5f80b6bp19f892jsn2819a0a0a90b"
+  }
+  @response2 = Unirest.get "https://the-cocktail-db.p.rapidapi.com/random.php",
+  headers:{
+    "X-RapidAPI-Host" => "the-cocktail-db.p.rapidapi.com",
+    "X-RapidAPI-Key" => "bcd50a1ebdmsh7fc3a20a5f80b6bp19f892jsn2819a0a0a90b"
+  }
+  @response3 = Unirest.get "https://the-cocktail-db.p.rapidapi.com/random.php",
+  headers:{
+    "X-RapidAPI-Host" => "the-cocktail-db.p.rapidapi.com",
+    "X-RapidAPI-Key" => "bcd50a1ebdmsh7fc3a20a5f80b6bp19f892jsn2819a0a0a90b"
+  }
+  @response4 = Unirest.get "https://the-cocktail-db.p.rapidapi.com/random.php",
+  headers:{
+    "X-RapidAPI-Host" => "the-cocktail-db.p.rapidapi.com",
+    "X-RapidAPI-Key" => "bcd50a1ebdmsh7fc3a20a5f80b6bp19f892jsn2819a0a0a90b"
+  }
+  @response5 = Unirest.get "https://the-cocktail-db.p.rapidapi.com/random.php",
+  headers:{
+    "X-RapidAPI-Host" => "the-cocktail-db.p.rapidapi.com",
+    "X-RapidAPI-Key" => "bcd50a1ebdmsh7fc3a20a5f80b6bp19f892jsn2819a0a0a90b"
+  }
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'recipemaster/index'
   root 'recipemaster#index'
   get "/dashboard" => "dashboard#index"
+  get "/cocktails" => "livedata#index"
 get "/maps" => "maps#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
