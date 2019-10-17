22f08e5cc75eb7b3a43402dcb633ce1520a78f18
I-Georg <i.georgieva.18@aberdeen.ac.uk>
2019-10-17 17:31:36 +0100

    leaflet working

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,17 @@
 class MapsController < ApplicationController
+#include Mapshelper
 def index
+@users =  User.all
+#@Latandlonshowing= latandlonshowing
+
+@users.each do |user|
+ #user.lat.to_f
+# user.lng.to_f
+ @lat = user.lat
+ @lon = user.lng
+
 end
+
+end
+
 end
