0970c5a9cd5affe5cca71d42958fba6daca8cebc
Sashfish <dormiii4@gmail.com>
2019-10-17 16:23:36 +0100

    basic search implemented

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,13 +2,15 @@ class Recipe < ApplicationRecord
 belongs_to :user, optional: true
 def self.search(search)
   if search
-    recipe = Recipe.where(name: search)
+    search = '%'+search+'%'
+    recipe = Recipe.where("name LIKE ?", search).limit(10)
     if recipe
-      self.where(name: recipe)
-    else Recipe.take(10)
+      self.where(id: recipe)
+    else
+      Recipe.take(10) #should be .all - shrunk to 10 for testing purposes
     end
   else
-    Recipe.take(10)
+    Recipe.take(10) #should be .all - shrunk to 10 for testing purposes
   end
 end
 end
