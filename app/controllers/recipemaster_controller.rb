27e310bf8bc0795fcd7fe65d1de05ab5b4bdc9ee
I-Georg <i.georgieva.18@aberdeen.ac.uk>
2019-10-18 22:17:32 +0100

    started implementing comparison

--- app/controllers/recipemaster_controller.rb
+++ app/controllers/recipemaster_controller.rb
@@ -1,2 +1,9 @@
 class RecipemasterController < ApplicationController
+def index
+@recipe1 = Recipe.where("n_ingredients = ?", params[:q]).take(2)
+@recipe2 = Recipe.where("n_ingredients = ?", params[:y]).take(2)
+
+
+end
+ 
 end
