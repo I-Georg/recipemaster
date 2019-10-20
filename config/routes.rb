c80dcf8e8ce1eb3f94ce7bf1727f028e5c01fde9
I-Georg <i.georgieva.18@aberdeen.ac.uk>
2019-10-20 19:36:41 +0100

    added upgraded comparison with upgraded search

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   resources :users
 resources :recipes, param: :recipe_id
 get '/displaycomparison' => 'recipecomparison#displaycomparison'
-
+get '/recipecomparison/search.json' => 'recipecomparison#search'
   get 'recipemaster/index'
   root 'recipemaster#index'
   get "/dashboard" => "dashboard#index"
   get "/cocktails" => "livedata#index"
   get "/recipecomparison" => "recipecomparison#index"
+ get "/recipecomparison/search" => "recipecomparison#search"
   get "/maps" => "maps#index"
   get "/allrecipes" => "recipes#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
