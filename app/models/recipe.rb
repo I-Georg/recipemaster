217bb3c4bb4efa5eedb6ef8eff88001118f2a5e8
Sashfish <dormiii4@gmail.com>
2019-10-22 10:26:07 +0100

    brushed things up for the presentation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,14 +4,14 @@ belongs_to :user, optional: true
 def self.search(search)
   if search
     search = '%'+search+'%'
-    recipe = Recipe.where("name LIKE ?", search).limit(10)
+    recipe = Recipe.where("name LIKE ?", search)
     if recipe
       self.where(id: recipe)
     else
-      Recipe.take(10) #should be .all - shrunk to 10 for testing purposes
+      Recipe.all #should be .all - shrunk to 10 for testing purposes
     end
   else
-    Recipe.take(10) #should be .all - shrunk to 10 for testing purposes
+    Recipe.all#should be .all - shrunk to 10 for testing purposes
   end
 end
 
